c3381e5d2ea61e350cc4f8213788b5ba445ef9cc
Ernesto <sereernest@gmail.com>
2025-12-10 06:24:07 +0300

    fix: update hardcode with dynamic values

--- checker.py
+++ checker.py
@@ -1,22 +1,27 @@
 import asyncio
+import os
+from typing import Optional
+
+from dotenv import load_dotenv
 import sigmax
 
-async def main():
-    # Создаем клиент внутри асинхронной функции
+load_dotenv()
+
+ACCESS_TOKEN: Optional[str] = os.getenv('ACCESS_TOKEN')
+
+async def main(access_token: str):
     client = sigmax.Client()
-    
-    # Логинимся перед выполнением запросов
-    await client.login_with_token("ТОКЕН")
+
+    await client.login_with_token(access_token)
     
     try:
-        # Выполняем запрос
         user = await client.get_user_by_phone('+78005553535')
         print(user.name)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
-    finally:
-        # Закрываем клиент при завершении
-        await client.close()
 
-# Запускаем основную функцию
-asyncio.run(main())
+if ACCESS_TOKEN:
+    print(ACCESS_TOKEN[:3])
+    asyncio.run(main(access_token=ACCESS_TOKEN))
+else:
+    raise ValueError('Set up the ACCESS_TOKEN first')
